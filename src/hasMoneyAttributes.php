eba56427ec140903f43647064be23de47d78f963
Irakli Kirkitadze <kirkita205@gmail.com>
2024-01-10 03:43:16 +0400

    Fix config reading

--- src/hasMoneyAttributes.php
+++ src/hasMoneyAttributes.php
@@ -12,12 +12,12 @@ trait hasMoneyAttributes
 
     protected function getCurrency(): Currency
     {
-        return $this->currency ?? $this->currency(config('app.currency') ?? Infolist::$defaultCurrency)->getCurrency();
+        return $this->currency ?? $this->currency(config('filament-money-field.default_currency') ?? Infolist::$defaultCurrency)->getCurrency();
     }
 
     protected function getLocale(): string
     {
-        return $this->locale ?? config('app.locale');
+        return $this->locale ?? config('filament-money-field.default_locale');
     }
 
     public function currency(string|\Closure|null $currencyCode = null): static
