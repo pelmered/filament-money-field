5b204640d550bfe877a506eb54a8ea01f228065c
Nick Breland <npbreland@tutanota.com>
2024-02-13 18:21:11 +0000

    fix: MoneyInput hydration

--- src/Forms/Components/MoneyInput.php
+++ src/Forms/Components/MoneyInput.php
@@ -15,16 +15,12 @@ class MoneyInput extends TextInput
     {
         parent::setUp();
 
-        $this->inputMode('decimal');
-        $this->step(0.01);
-        $this->minValue = 0;
-
         $this->formatStateUsing(function (MoneyInput $component, $state): ?string {
             
-            $currency = $component->getCurrency()->getCode();
-            $state = MoneyFormatter::parseDecimal($state, $currency, $component->getLocale());
-          
             $this->prepare($component);
+
+            $currency = $component->getCurrency();
+            $locale = $component->getLocale();
 
             if (is_null($state)) {
                 return '';
@@ -33,15 +29,15 @@ class MoneyInput extends TextInput
                 return $state;
             }
 
-            return MoneyFormatter::decimalToMoneyString($state / 100, $component->getLocale());
+            return MoneyFormatter::format($state, $currency, $locale);
         });
 
         $this->dehydrateStateUsing(function (MoneyInput $component, $state): string {
 
-            $this->prepare($component);
-
             $currency = $component->getCurrency()->getCode();
             $state = MoneyFormatter::parseDecimal($state, $currency, $component->getLocale());
+
+            $this->prepare($component);
 
             return $state;
         });
@@ -50,15 +46,9 @@ class MoneyInput extends TextInput
     protected function prepare(MoneyInput $component): void
     {
         $formattingRules = MoneyFormatter::getFormattingRules($component->getLocale());
-
         $this->prefix($formattingRules->currencySymbol);
-
         if (config('filament-money-field.use_input_mask')) {
             $this->mask(RawJs::make('$money($input, \'' . $formattingRules->decimalSeparator . '\', \'' . $formattingRules->groupingSeparator . '\', ' . $formattingRules->fractionDigits . ')'));
         }
-
-        $this->stripCharacters($formattingRules->groupingSeparator);
-        // OR
-        $this->stripCharacters([',', '.', ' ',]);
     }
 }
