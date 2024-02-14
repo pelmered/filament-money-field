b6bc755a8c5c3f8e6cb10ff281f4388236162f05
Nick Breland <npbreland@tutanota.com>
2024-02-14 12:41:10 +0000

    feat: max & min validation

--- src/Forms/Components/MoneyInput.php
+++ src/Forms/Components/MoneyInput.php
@@ -47,8 +47,49 @@ class MoneyInput extends TextInput
     {
         $formattingRules = MoneyFormatter::getFormattingRules($component->getLocale());
         $this->prefix($formattingRules->currencySymbol);
+
         if (config('filament-money-field.use_input_mask')) {
             $this->mask(RawJs::make('$money($input, \'' . $formattingRules->decimalSeparator . '\', \'' . $formattingRules->groupingSeparator . '\', ' . $formattingRules->fractionDigits . ')'));
         }
+    }
+
+    public function minValue(mixed $min): static
+    {
+        $this->rule(static function (MoneyInput $component, mixed $state) use ($min) {
+            return function (string $attribute, mixed $value, \Closure $fail) use ($component, $state, $min) {
+
+                $value = MoneyFormatter::parseDecimal(
+                    $state, 
+                    $component->getCurrency()->getCode(),
+                    $component->getLocale()
+                );
+
+                if ($value < $min) {
+                    $fail('The :attribute must be greater than or equal to ' . $min . '.');
+                }
+            };
+        });
+
+        return $this;
+    }
+
+    public function maxValue(mixed $max): static
+    {
+        $this->rule(static function (MoneyInput $component, mixed $state) use ($max) {
+            return function (string $attribute, mixed $value, \Closure $fail) use ($component, $state, $max) {
+
+                $value = MoneyFormatter::parseDecimal(
+                    $state, 
+                    $component->getCurrency()->getCode(),
+                    $component->getLocale()
+                );
+
+                if ($value > $max) {
+                    $fail('The :attribute must be less than or equal to ' . $max . '.');
+                }
+            };
+        });
+
+        return $this;
     }
 }
