1d87f737840a491db177f063b2699821ad8d8b74
Peter Elmered <peter@elmered.com>
2024-05-08 20:36:50 +0200

    Set validation classes as read only

--- src/Forms/Rules/MaxValueRule.php
+++ src/Forms/Rules/MaxValueRule.php
@@ -7,7 +7,7 @@ use Money\Exception\ParserException;
 use Pelmered\FilamentMoneyField\Forms\Components\MoneyInput;
 use Pelmered\FilamentMoneyField\MoneyFormatter;
 
-readonly class MaxValueRule implements ValidationRule
+class MaxValueRule implements ValidationRule
 {
     public function __construct(private int $max, private MoneyInput $component)
     {}
