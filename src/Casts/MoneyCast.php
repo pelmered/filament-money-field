886de6a87d671c9e76750aef571ff40ff506ac34
Peter Elmered <peter@elmered.com>
2024-11-07 03:31:56 +0100

    Add money cast and livewire synthesizer

---
+++ src/Casts/MoneyCast.php
@@ -0,0 +1,31 @@
+<?php
+namespace Pelmered\FilamentMoneyField\Casts;
+
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+use Money\Currency;
+use Money\Money;
+
+class MoneyCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): Money
+    {
+        return new Money($value, new Currency('USD'));
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return $value;
+    }
+}
