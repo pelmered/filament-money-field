506cfb1c1bc74989a83697027dead1ecb97cdaab
Peter Elmered <peter@elmered.com>
2025-03-28 11:22:02 +0100

    WIP

--- src/Currencies/Providers/ISOCurrenciesProvider.php
+++ src/Currencies/Providers/ISOCurrenciesProvider.php
@@ -12,17 +12,6 @@ class ISOCurrenciesProvider implements CurrenciesProvider
     {
         $file = base_path('vendor/moneyphp/money/resources/currency.php');
 
-        $f = dirname($file);
-
-        for($i = 5; $i > 1; $i--) {
-            dump(scandir(dirname($f, $i)));
-        }
-
-        //$files1 = scandir(dirname($f, 2));
-        $files2 = scandir(dirname($f, 5));
-
-        dump($f, is_dir($f), $files2);
-
         if (is_file($file)) {
             return require $file;
         }
