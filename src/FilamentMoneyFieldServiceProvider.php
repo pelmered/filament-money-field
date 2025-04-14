44b780aa739ed78432102d9146ed876d35ba69d3
Gerardo Ibarra <ibarragp@gmail.com>
2025-04-14 11:27:15 -0300

    refactor configure and boot service provider (spatie)

--- src/FilamentMoneyFieldServiceProvider.php
+++ src/FilamentMoneyFieldServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Pelmered\FilamentMoneyField;
 
 use Livewire\Livewire;
+use Pelmered\LaraPara\Commands\CacheCommand;
+use Pelmered\LaraPara\Commands\ClearCacheCommand;
+use Pelmered\LaraPara\Currencies\CurrencyCollection;
 use Pelmered\FilamentMoneyField\Synthesizers\CurrencySynthesizer;
 use Pelmered\FilamentMoneyField\Synthesizers\MoneySynthesizer;
 use Spatie\LaravelPackageTools\Package;
@@ -16,14 +19,38 @@ class FilamentMoneyFieldServiceProvider extends PackageServiceProvider
     {
         $package->name(static::$name)
             ->hasConfigFile()
-            ->hasTranslations();
+            ->hasTranslations()
+            ->hasCommands([
+                CacheCommand::class,
+                ClearCacheCommand::class,
+            ]);
     }
 
     public function boot(): void
     {
         parent::boot();
 
+        // Requires Laravel 11.27.1
+        // See: https://github.com/laravel/framework/pull/52928
+        /** @phpstan-ignore function.alreadyNarrowedType  */
+        if (method_exists($this, 'optimizes')) {
+            $this->optimizes(
+                optimize: CacheCommand::class,
+                clear: ClearCacheCommand::class,
+            );
+        }
+
         Livewire::propertySynthesizer(CurrencySynthesizer::class);
         Livewire::propertySynthesizer(MoneySynthesizer::class);
+    }
+
+    public function register(): void
+    {
+        parent::register();
+
+        $this->app->bind(CurrencyCollection::class, function (): CurrencyCollection {
+            return new CurrencyCollection;
+        });
+
     }
 }
